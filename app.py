b68a42b005fa8d3f1f5e955a3f80fe2b93b1e1ee
ChPurna2003 <chpurna2003@gmail.com>
2025-11-19 13:26:05 +0530

    Refactor analyze route and update server port
    Refactor analyze route to remove POST handling and update app run configuration.

--- app.py
+++ app.py
@@ -1,50 +1,29 @@
-from flask import Flask, render_template, request, jsonify
-import requests
-import os
+from flask import Flask, render_template, request, jsonify, redirect
+import asyncio, os
+
+from inference_sdk import InferenceHTTPClient
+CLIENT = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key="kTK0gAawSv3eqEDeXfAs"
+)
 
 app = Flask(__name__)
-
-# ------------------------------
-# Roboflow model endpoint
-# ------------------------------
-API_KEY = "kTK0gAawSv3eqEDeXfAs"
-MODEL_ENDPOINT = "https://detect.roboflow.com/skin-disease-8class/1"
-
 
 @app.route("/")
 def dash():
     return render_template('analyze.html')
 
-
-@app.route("/analyze", methods=["GET", "POST"])
+@app.route("/analyze")
 def analyze():
-    if request.method == "POST":
+    return render_template('app.html')
+    
 
-        if "file" not in request.files:
-            return jsonify({"error": "No file received"})
-
-        file = request.files["file"]
-        temp_path = "temp.jpg"
-        file.save(temp_path)
-
-        # send to roboflow
-        url = f"{MODEL_ENDPOINT}?api_key={API_KEY}"
-        response = requests.post(url, files={"file": open(temp_path, "rb")})
-
-        os.remove(temp_path)
-
-        try:
-            return jsonify(response.json())
-        except:
-            return jsonify({"error": "Prediction failed"})
-
-    return render_template("app.html")
-
+UPLOAD_FOLDER = '/img'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/favicon.ico')
 def favicon():
-    return jsonify({"msg": "ok"})
-
+    return jsonify(1)
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=5000)
+   app.run(host='0.0.0.0', port=5001, debug=True)
